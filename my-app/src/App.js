76886b55428647d49b7152009c89fd1b0c4f2334
deckurodev <sagada1628@gmail.com>
2025-04-02 16:29:17 +0900

    D

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction dddddddddddd
+          Learn GithubAction dz
         </a>
       </header>
     </div>
