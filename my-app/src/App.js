ceb7e7cdb5492227b9864d35c3acc2153b27bb71
deckurodev <126306475+deckurodev@users.noreply.github.com>
2025-04-02 22:05:05 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction dz
+          Learn GithubAction dz3312321
         </a>
       </header>
     </div>
